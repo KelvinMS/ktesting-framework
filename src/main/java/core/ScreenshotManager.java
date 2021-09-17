fed909905576fe1a5d7704f5e80cdb2cc818eaed
Kelvin M <kelvin651b@gmail.com>
2021-09-17 01:51:18 -0300

    Adjust class to use Paths class

--- src/main/java/core/ScreenshotManager.java
+++ src/main/java/core/ScreenshotManager.java
@@ -6,19 +6,21 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.io.FileHandler;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class ScreenshotManager {
 
     //TODO IMPORTANTE
     // https://www.tutorialspoint.com/create-a-temporary-file-in-java#:~:text=A%20temporary%20file%20can%20be,createTempFile().
-    private static final String FILEPATH = System.getProperty("user.dir") + "/target/ExtentReport";
+    private static final String SCREENSHOTPATH = Paths.get(System.getProperty("user.dir"),"target","ExtentReport").toString();
     private static final String FILENAME = "screenshot.png";
     public static String imagemBASE64;
 
     public void takeScreenShot(WebDriver driver) {
         try {
             File imagem = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-            FileHandler.copy(imagem, new File(FILEPATH+"/" + FILENAME));
+            FileHandler.copy(imagem, new File(Paths.get(SCREENSHOTPATH,FILENAME).toString()));
         } catch (IOException e) {
             e.printStackTrace();
             System.out.println("Error trying to take screenshot...");
