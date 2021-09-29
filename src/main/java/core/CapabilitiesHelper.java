89da7b74847b7d534b05bfb51e302e2cf718bf1c
Kelvin M <kelvin.moreira@westwing.com.br>
2021-09-29 01:08:21 -0300

    Creation of static variables

---
+++ src/main/java/core/CapabilitiesHelper.java
@@ -0,0 +1,18 @@
+package core;
+
+public class CapabilitiesHelper {
+
+    public static class Westwingclub {
+        public static final String APP_PACKAGE_STAGE = "br.westwing.android";
+        public static final String APP_PACKAGE_LIVE = "br.westwing.android";
+        public static final String MAIN_ACTIVITY = ".presentation.activities.SplashActivity";
+        public static final String BUNDLE_ID = "";
+    }
+
+    public static class Platform {
+        public static final String PLATFORM_NAME_IOS = "ios";
+        public static final String PLATFORM_NAME_ANDROID = "android";
+        public static final String AUTOMATION_NAME_IOS = "XCUITest";
+        public static final String AUTOMATION_NAME_ANDROID = "uiautomator2";
+    }
+}
